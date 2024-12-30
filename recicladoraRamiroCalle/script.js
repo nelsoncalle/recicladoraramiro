145798228f9f1eebdf8b955a82983d9d704e1425
Nelson <nrcalle_790@hotmail.com>
2024-12-30 16:20:56 -0500

    ok

--- recicladoraRamiroCalle/script.js
+++ recicladoraRamiroCalle/script.js
@@ -1,11 +1,3 @@
-// document.addEventListener('DOMContentLoaded', function() { 
-//     const iconoHamburguesa = document.getElementById('icono-hamburguesa'); 
-//     const menu = document.getElementById('menu'); 
-//     iconoHamburguesa.addEventListener('click', function() { 
-//         menu.classList.toggle('active'); 
-//     });
-// });
-
 document.addEventListener('DOMContentLoaded', function() { 
     const iconoHamburguesa = document.getElementById('icono-hamburguesa'); 
     const menu = document.getElementById('menu'); 
