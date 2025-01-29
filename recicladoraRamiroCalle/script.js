2405b1eed820b5875d36c90d8d7db18d7069a966
Nelson <nrcalle_790@hotmail.com>
2025-01-29 16:10:56 -0500

    g

--- recicladoraRamiroCalle/script.js
+++ recicladoraRamiroCalle/script.js
@@ -9,15 +9,15 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
-const carousel = document.querySelector('.carousel-inner');
-let index = 0;
+// const carousel = document.querySelector('.carousel-inner');
+// let index = 0;
 
-function slide() {
-    index++;
-    if (index >= 4) {
-        index = 0;
-    }
-    carousel.style.transform = `translateX(${-index * 100}%)`;
-}
+// function slide() {
+//     index++;
+//     if (index >= 4) {
+//         index = 0;
+//     }
+//     carousel.style.transform = `translateX(${-index * 100}%)`;
+// }
 
-setInterval(slide, 4000);
+// setInterval(slide, 4000);
